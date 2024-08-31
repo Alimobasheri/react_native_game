a337ae5adf857100bd6ebefd9472c3aa69bd44a9
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-08-31 18:50:58 +0330

    feat(useGameLoop, rnsge): Add start and stop features
    - feat(useGameLoop): Add functions to control start and stopping game updates.
    - feat(useGameLoop): Add options props to control initial running state of the game engine
    - feat(RNSGE): Add ref and imperative handle to control game engine start and stop states.
    - feat(RNSGE): Add options props to pass to the game loop

--- src/containers/ReactNativeSkiaGameEngine/RNSGE.tsx
+++ src/containers/ReactNativeSkiaGameEngine/RNSGE.tsx
@@ -1,10 +1,19 @@
-import { FC, memo, PropsWithChildren, useMemo, useState } from 'react';
+import {
+  FC,
+  forwardRef,
+  memo,
+  PropsWithChildren,
+  useImperativeHandle,
+  useMemo,
+  useState,
+} from 'react';
 import {
   useEntities,
   useGameLoop,
   useSystems,
   useTouchService,
   useDispatcher,
+  UseGameLoopOptions,
 } from './hooks';
 import { RNSGEContext } from './context';
 import { MemoizedContainer } from './components/MemoizedContainer';
@@ -15,56 +24,83 @@ import {
 } from 'react-native-gesture-handler';
 import { OnEventListeners } from './types/Events';
 
-interface RNSGEProps extends PropsWithChildren {
-  onEventListeners?: OnEventListeners;
+/**
+ * This is the handle for the `ReactNativeSkiaGameEngine` component. Use this to control the game engine.
+ * @interface RNSGEHandle
+ * @property {() => void} start - start the game engine
+ * @property {() => void} stop - stop the game engine
+ */
+export interface RNSGEHandle {
+  start: () => void;
+  stop: () => void;
 }
 
-export const ReactNativeSkiaGameEngine: FC<RNSGEProps> = memo(
-  ({ children, onEventListeners }) => {
-    const entities = useEntities();
-    const systems = useSystems();
-    const dispatcher = useDispatcher();
-    const touchService = useTouchService();
-    const { frames } = useGameLoop(
-      entities,
-      systems,
-      dispatcher,
-      onEventListeners
-    );
-    const [dimensions, setDimensions] = useState<{
-      width: number | null;
-      height: number | null;
-    }>({ width: null, height: null });
-    const value = useMemo(
-      () => ({
-        entities: entities,
-        systems: systems,
-        frames: frames,
+/**
+ * Props for the `ReactNativeSkiaGameEngine` component
+ * @property {OnEventListeners} onEventListeners - The event listeners to be registered
+ * @property {UseGameLoopOptions} gameLoopOptions - Options for controlling the Game Loop
+ * @property {ReactNode} children - The game components
+ */
+interface RNSGEProps extends PropsWithChildren {
+  onEventListeners: OnEventListeners;
+  gameLoopOptions?: UseGameLoopOptions;
+}
+
+export const ReactNativeSkiaGameEngine = forwardRef<RNSGEHandle, RNSGEProps>(
+  memo(
+    ({ children, onEventListeners = {}, gameLoopOptions }: RNSGEProps, ref) => {
+      const entities = useEntities();
+      const systems = useSystems();
+      const dispatcher = useDispatcher();
+      const touchService = useTouchService();
+      const { frames, start, stop } = useGameLoop(
+        entities,
+        systems,
         dispatcher,
-        touchService,
-        dimensions,
-      }),
-      [touchService.gestures, dimensions]
-    );
-    return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <GestureDetector gesture={touchService.gestures}>
-          <Canvas
-            style={{ flex: 1 }}
-            onLayout={({
-              nativeEvent: {
-                layout: { width, height },
-              },
-            }) => setDimensions({ width, height })}
-          >
-            <RNSGEContext.Provider value={value}>
-              {dimensions.width !== null && dimensions.height !== null && (
-                <MemoizedContainer>{children}</MemoizedContainer>
-              )}
-            </RNSGEContext.Provider>
-          </Canvas>
-        </GestureDetector>
-      </GestureHandlerRootView>
-    );
-  }
+        onEventListeners,
+        gameLoopOptions
+      );
+      const [dimensions, setDimensions] = useState<{
+        width: number | null;
+        height: number | null;
+      }>({ width: null, height: null });
+      const value = useMemo(
+        () => ({
+          entities: entities,
+          systems: systems,
+          frames: frames,
+          dispatcher,
+          touchService,
+          dimensions,
+        }),
+        [touchService.gestures, dimensions]
+      );
+
+      useImperativeHandle(ref, () => ({
+        start,
+        stop,
+      }));
+
+      return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <GestureDetector gesture={touchService.gestures}>
+            <Canvas
+              style={{ flex: 1 }}
+              onLayout={({
+                nativeEvent: {
+                  layout: { width, height },
+                },
+              }) => setDimensions({ width, height })}
+            >
+              <RNSGEContext.Provider value={value}>
+                {dimensions.width !== null && dimensions.height !== null && (
+                  <MemoizedContainer>{children}</MemoizedContainer>
+                )}
+              </RNSGEContext.Provider>
+            </Canvas>
+          </GestureDetector>
+        </GestureHandlerRootView>
+      );
+    }
+  )
 );
