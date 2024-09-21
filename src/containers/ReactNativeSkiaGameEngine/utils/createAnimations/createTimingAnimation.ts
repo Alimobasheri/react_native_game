830f660bb64db388ab20ae7feb73853ee94bf255
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-09-21 20:26:45 +0330

    fix(engine.aniamtions): The yoyo effect didn't work correctly in timing animation.
    - Fixed it by removing unnecessary directin multiplier inside timing function

--- src/containers/ReactNativeSkiaGameEngine/utils/createAnimations/createTimingAnimation.ts
+++ src/containers/ReactNativeSkiaGameEngine/utils/createAnimations/createTimingAnimation.ts
@@ -2,30 +2,21 @@ import { runOnJS } from 'react-native-reanimated';
 import { Animation } from '../../services/Animations';
 import { linear } from '../easings';
 
-const log = (value: any) => console.log(value);
-
 export function createTimingAnimation(
   startValue: number,
   targetValue: number,
   duration: number,
   easing: (progress: number) => number = linear
 ): Animation {
-  //@ts-ignore
-  const startTime = global.nativePerformanceNow();
-
   return {
     update: (now, sharedValue, progress, isBackward, onAnimate) => {
       'worklet';
-      //@ts-ignore
-      const elapsedTime = now - startTime;
 
       const easedProgress = easing(progress);
-      const directionMultiplier = isBackward ? -1 : 1;
 
       // Update the shared value
       sharedValue.value =
-        startValue +
-        easedProgress * directionMultiplier * (targetValue - startValue);
+        startValue + easedProgress * (targetValue - startValue);
 
       const done = isBackward ? progress <= 0 : progress >= 1;
 
