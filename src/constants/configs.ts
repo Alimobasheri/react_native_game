ab96f4c895ab592153f1f79286ef6933fd836ff3
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-09-22 23:29:53 +0330

    - feat(game.entities): Add State entity with isRunning which will stop and start physics system. We'll modify and improve this state in future updates.

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,32 +1,33 @@
-import { SeaConfig } from "@/Game/Entities/Sea/types";
-import { WATER_GRADIENT_COLORS } from "./waterConfigs";
+import { SeaConfig } from '@/Game/Entities/Sea/types';
+import { WATER_GRADIENT_COLORS } from './waterConfigs';
 
 export const TRAIL_FADE_DURATION = 1000;
 
 export enum ENTITIES_KEYS {
-  STARS = "stars",
-  MOON = "moon",
-  CLOUDS = "clouds",
-  SEA = "sea",
-  SHIP = "ship",
-  BOAT_LABEL = "boat_",
-  SEA_GROUP = "sea_group",
-  BOAT_GROUP = "boat_group",
-  MOUNTAIN_BACKGROUND = "mountain_background",
-  GAME_LOOP_SYSTEM = "game_loop_system",
-  TOUCH_SYSTEM_INSTANCE = "touch_system_instance",
-  BACKGROUND_SYSTEM_INSTANCE = "background_system_instance",
-  SEA_SYSTEM_INSTANCE = "sea_system_instance",
-  SHIP_SYSTEM_INSTANCE = "ship_system_instance",
-  PHYSICS_SYSTEM_INSTANCE = "physics_system_instance",
-  COLLISIONS_SYSTEM_INSTANCE = "collisions_system_instanc",
-  BOAT_SYSTEM_INSTANCE = "boat_system_instance",
-  UI_SYSTEM_INSTANCE = "ui_system_instance",
-  SCREEN_TOP_UI = "screen_top_ui",
+  STARS = 'stars',
+  MOON = 'moon',
+  CLOUDS = 'clouds',
+  SEA = 'sea',
+  SHIP = 'ship',
+  BOAT_LABEL = 'boat_',
+  SEA_GROUP = 'sea_group',
+  BOAT_GROUP = 'boat_group',
+  MOUNTAIN_BACKGROUND = 'mountain_background',
+  GAME_LOOP_SYSTEM = 'game_loop_system',
+  TOUCH_SYSTEM_INSTANCE = 'touch_system_instance',
+  BACKGROUND_SYSTEM_INSTANCE = 'background_system_instance',
+  SEA_SYSTEM_INSTANCE = 'sea_system_instance',
+  SHIP_SYSTEM_INSTANCE = 'ship_system_instance',
+  PHYSICS_SYSTEM_INSTANCE = 'physics_system_instance',
+  COLLISIONS_SYSTEM_INSTANCE = 'collisions_system_instanc',
+  BOAT_SYSTEM_INSTANCE = 'boat_system_instance',
+  UI_SYSTEM_INSTANCE = 'ui_system_instance',
+  SCREEN_TOP_UI = 'screen_top_ui',
+  STATE = 'state',
 }
 
-export const VEHICLES_GROUP = "vehicles";
-export const BUOYANTS_GROUP = "buoyants";
+export const VEHICLES_GROUP = 'vehicles';
+export const BUOYANTS_GROUP = 'buoyants';
 
 export function getSeaConfigDefaults(
   windowWidth: number,
@@ -47,8 +48,8 @@ export function getSeaConfigDefaults(
 }
 
 export enum DIRECTION {
-  UP = "up",
-  DOWN = "down",
-  LEFT = "left",
-  RIGHT = "right",
+  UP = 'up',
+  DOWN = 'down',
+  LEFT = 'left',
+  RIGHT = 'right',
 }
