a974e4e1f4803da05f0a55d53b470a31e337bd4c
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-09-27 17:03:48 +0330

    feat(rnsge.useTouchHandler): Add the callbacks for the new CRUD functions of tocuhService

--- src/containers/ReactNativeSkiaGameEngine/hooks/useTouchHandler.ts
+++ src/containers/ReactNativeSkiaGameEngine/hooks/useTouchHandler.ts
@@ -1,16 +1,36 @@
-import { useCallback, useContext, useEffect } from "react";
-import { RNSGEContext } from "../context/RNSGEContext";
-import { GestureType } from "react-native-gesture-handler";
+import { useCallback, useContext, useEffect } from 'react';
+import { RNSGEContext } from '../context/RNSGEContext';
+import { GestureType } from 'react-native-gesture-handler';
 
 export const useTouchHandler = (gesture?: GestureType) => {
   const { touchService } = useContext(RNSGEContext);
 
   useEffect(() => {
-    if (gesture) touchService.addGesture(gesture);
-  }, [gesture]);
+    if (gesture) {
+      touchService.addGesture(gesture);
+    }
+  }, [gesture, touchService]);
 
-  const addGesture = useCallback((gesture: GestureType) => {
-    touchService.addGesture(gesture);
-  }, []);
-  return { addGesture };
+  const addGesture = useCallback(
+    (gesture: GestureType) => {
+      return touchService.addGesture(gesture);
+    },
+    [touchService]
+  );
+
+  const removeGesture = useCallback(
+    (identifier: string | { label?: string; groups?: string[] }) => {
+      touchService.removeGesture(identifier);
+    },
+    [touchService]
+  );
+
+  const updateGesture = useCallback(
+    (id: string, gesture: GestureType) => {
+      touchService.updateGesture(id, gesture);
+    },
+    [touchService]
+  );
+
+  return { addGesture, removeGesture, updateGesture };
 };
