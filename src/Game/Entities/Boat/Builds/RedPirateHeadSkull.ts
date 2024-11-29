985745eed0343e6b4505c87e0299abd9a0ff3c44
Mir Ali Mobasheri <Alimobasheri@users.noreply.github.com>
2024-11-29 17:39:47 +0330

    fix(game.entity): Improve boat speed

--- src/Game/Entities/Boat/Builds/RedPirateHeadSkull.ts
+++ src/Game/Entities/Boat/Builds/RedPirateHeadSkull.ts
@@ -1,15 +1,15 @@
-import { Boat } from "../Boat";
-import { BoatConfig, commonBoatBuildsConfig } from "../types";
+import { Boat } from '../Boat';
+import { BoatConfig, commonBoatBuildsConfig } from '../types';
 
 export class RedPirateHeadSkull extends Boat {
   protected _sizeRatio = 308 / 512;
   protected _windowWidth: number;
-  protected _imageSource: string = "skull-head-pirate-shark-red.png";
+  protected _imageSource: string = 'skull-head-pirate-shark-red.png';
   constructor(config: commonBoatBuildsConfig) {
     super(config);
     this._windowWidth = config.windowWidth;
     this.initialize();
-    this._acceleration = 0.01;
+    this._acceleration = 0.2;
   }
   public getSize(): number[] {
     const size = [
